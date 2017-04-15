35e9d1d2d1e8b0924035b3eece4b57bfbe8714b3
mtso <mtso@tuta.io>
2017-04-15 13:29:00 -0700

    Scale axis

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 ~function() {
   const d3 = require('d3')
   const config = require('./config')
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ]
 
   function display() {
     let gdp = JSON.parse(this.responseText)
@@ -16,7 +30,7 @@
       width: 1000,
       height: 600,
     }
-    let padding = 30
+    let padding = 40
 
     let svg =
       d3.select('body')
@@ -25,22 +39,37 @@
         .attr('height', size.height)
         .attr('class', 'container')
 
-    // var tt = svg.append('text')
-    //             .attr('class', 'tooltip')
-
     let minDate = d3.min(data, d => d.date)
     let maxDate = d3.max(data, d => d.date)
     let xScale = d3.scaleLinear()
                    .domain([minDate, maxDate])
-                   .range([padding, size.width - padding])
+                   .range([padding, size.width - padding * 3])
 
     let yScale = d3.scaleLinear()
                    .domain([d3.min(data, d => d.gdp), d3.max(data, d => d.gdp)])
-                   .range([size.height - padding, padding])
+                   .range([size.height - padding , padding * 3])
+
+    let xScaleAxis = d3.scaleLinear()
+                   .domain([minDate.getFullYear(), maxDate.getFullYear()])
+                   .range([padding, size.width - padding * 3])
+
+    let xAxis = d3.axisBottom(xScaleAxis)
+    let yAxis = d3.axisLeft(yScale)
 
     var tt;
-    // let tt = d3.select('body')
-    //   .append('div')
+
+    let ttsize = {
+      width: 100,
+      height: 40
+    }
+
+    svg.append('g')
+      .attr('transform', 'translate(0, ' + (size.height - padding) + ')')
+      .call(xAxis)
+
+    svg.append('g')
+      .attr('transform', 'translate(' + padding + ', 0)')
+      .call(yAxis)
 
     let bars = svg.selectAll('rect')
        .data(data)
@@ -48,30 +77,87 @@
        .append('rect')
 
        .attr('width', (size.width - padding * 2) / data.length)
-       .attr('height', d => d.gdp)
+       .attr('height', d => size.height - yScale(d.gdp))
        .attr('x', d => xScale(d.date))
-       .attr('y', d => yScale(d.gdp))
+       .attr('y', d => yScale(d.gdp) - padding)
        .attr('class', 'bar')
+
+
        .on('mouseover', (d) => {
-         tt = d3
-           .select('body')
-           .append('div')
-           .style('left', xScale(d.date) + 'px')
-           .style('top', yScale(d.gdp) + 'px')
-           .attr('class', 'tooltip')
-           .html(d.gdp + '<br>' + d.date.getFullYear())
-          //  .text(d.gdp + d.date)
-        //  tt = svg
-        //    .append('text')
-        //    .attr('class', 'tooltip')
-        //    .text(d.gdp)
-        //
-        //  tt.attr('x', xScale(d.date))
-        //    .attr('y', yScale(d.gdp))
+         tt = svg.append('g')
+
+         tt.append('rect')
+           .attr('x', xScale(d.date))
+           .attr('y', yScale(d.gdp) - padding * 2)
+           .attr('width', 100)
+           .attr('height', 40)
+           .attr('fill', 'black')
+
+         tt.append('text')
+           .text(d.gdp)
+           .attr('x', xScale(d.date) + 5)
+           .attr('y', yScale(d.gdp) - 24 - padding)
+           .attr('fill', 'white')
+
+         tt.append('text')
+           .text(d.date.getFullYear())
+           .attr('x', xScale(d.date) + 5)
+           .attr('y', yScale(d.gdp) - 10 - padding)
+           .attr('fill', 'white')
        })
-       .on('mouseout', () => {
+       .on('mouseout', d => {
          tt.remove()
        })
+
+      //  .append('svg:title')
+      //  .text(d => 'GDP: ' + d.gdp + ' Billion\n' + months[d.date.getMonth()] + ' ' + d.date.getFullYear())
+
+
+      //  .on('mouseover', (d) => {
+      //    tt = svg.append('svg:title')
+      //      .text(d.gdp + '\n' + d.date.getFullYear())
+      //   //  tt = d3.select('body')
+      //   //    .append('div')
+      //   //
+      //   //  tt = svg
+      //   //    .append('rect')
+      //   //    .attr('width', ttsize.width)
+      //   //    .attr('height', ttsize.height)
+      //   //    .attr('fill', 'gray')
+      //   //    .attr('x', xScale(d.date))
+      //   //    .attr('y', yScale(d.gdp) - 100 + 2)
+       //
+       //
+      //     //  .attr('opacity', 0.3)
+      //   //  tt
+      //   //    .append('text')
+      //   //    .text(d.gdp + '\n' + d.date.getFullYear())
+      //   //    .attr('fill', 'red')
+      //   //    .attr('font-size', '60px')
+      //   //   //  .attr('fill', 'white')
+      //   //   //  .attr('class', 'tooltip-text')
+      //   //   //  .append('rect')
+      //   //   // //  .attr('fill', 'black')
+      //   //   //  .append('text')
+      //   //   //  .text(d.gdp + '\n' + d.date.getFullYear())
+      //   //   //  .attr('x', xScale(d.date))
+      //   //   //  .attr('y', yScale(d.gdp))
+      //    //
+      //    //
+      //   //   //  .attr('font-color', 'white')
+      //   //   //  .select('body')
+      //   //   //  .select()
+      //   //   // svg
+      //   //   //  .append('div')
+      //   //   //
+      //   //   //  .style('left', xScale(d.date) + 8 + 'px')
+      //   //   //  .style('top', (yScale(d.gdp) - 46) + 'px')
+      //   //   //  .attr('class', 'tooltip')
+      //   //   //  .html(d.gdp + '<br>' + d.date.getFullYear())
+      //  })
+      //  .on('mouseout', () => {
+      //    tt.remove()
+      //  })
   }
 
   var req = new XMLHttpRequest()
